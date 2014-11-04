3d0d15479f1740c6ae43b6927feda64abbe2f5d5
Tiamanti <michal.szura@gmail.com>
2014-11-04 09:59:40 +0100

    Class Structure Setup v1

---
+++ Bakterie/src/pl/edu/uj/fais/amsi/bio/Worm.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pl.edu.uj.fais.amsi.bio;
+
+import pl.edu.uj.fais.amsi.map.MapObject;
+
+/**
+ *
+ * @author student
+ */
+public class Worm extends MapObject{
+    
+}
---
+++ Bakterie/src/pl/edu/uj/fais/amsi/main/Game.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.edu.uj.fais.amsi.main;
+
+/**
+ *
+ * @author student
+ */
+public class Game {
+
+    private static Long x = System.nanoTime();
+
+    public static void main(String[] args) {
+    }
+
+    //Very Fast Random
+    public static long randomLong() {
+        x ^= (x << 21);
+        x ^= (x >>> 35);
+        x ^= (x << 4);
+        return x;
+    }
+
+}
